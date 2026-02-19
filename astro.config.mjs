8d9f1679cfbc5bdf58a1b3c8b513e9118ae0a513
Misato Arai <misato.arai0204@gmail.com>
2026-02-19 15:54:59 +0900

    preactの追加と挨拶用バナーを追加。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://famous-meerkat-50d749.netlify.app/",
+  integrations: [preact()],
 });
